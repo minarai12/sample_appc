5c05fe9105b0953b86df0c1f5577a76534326f9a
Agnieszka Lewicka <minarai12@gmail.com>
2012-08-04 17:20:16 +0200

    czesciowa autoryzacja

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,6 +22,23 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def signed_in_user
+    redirect_to signin_path, notice: "Please sign in." unless signed_in?
   end
 
 end
