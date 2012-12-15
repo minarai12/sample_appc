3e2342869d116777264caa7598139f88ed0079bf
Agnieszka Lewicka <minarai12@gmail.com>
2012-12-15 20:35:44 +0100

    sledzenie

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -98,6 +98,15 @@ describe "Authentication" do
         end
       end
 
+      describe "visiting the following page" do
+        before { visit following_user_path(user) }
+        it { should have_selector('title', text: 'Sign in') }
+      end
+
+      describe "visiting the followers page" do
+        before { visit followers_user_path(user) }
+        it { should have_selector('title', text: 'Sign in') }
+      end
     end
 
 
